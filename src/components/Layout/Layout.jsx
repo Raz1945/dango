992ba502a1aac9f7c9eaea5cab4e5d7c157d2559
raz1945 <g.sebas.sanchez@gmail.com>
2024-11-27 11:40:01 -0300

    Modal

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import { Outlet } from "react-router-dom"
 import { Navbar } from '../Navbar/Navbar'
-import { Banner } from '../Banner'
 
 import './Layout.css'
 export const Layout = () => {
   return (
     <>
-      <Banner />
       <Navbar />
       <Outlet />
     </>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,32 +1,24 @@
-import React from 'react';
-import { HiBars3 } from "react-icons/hi2";
+import React from "react";
+import { Banner } from '../Banner/index';
+import { Nav } from './Nav';
 import { Tienda } from '../Icon/Tienda';
-import { NavbarItem } from './NavbarItem';
+
 import '../Navbar/index.css';
 
 export const Navbar = () => {
+
+
   return (
-    <nav className="navbar">
-      <HiBars3 
-        className="navbar__menu-icon" 
-        color="black" 
-        size={20} 
-        onClick={() => console.log("Abrir menú")} // todo 
-      />
+      <>
+        <Banner />
 
-      <ul className="navbar__list">
-        <NavbarItem to="/home" label="Home" />
-        <NavbarItem to="/item1" label="Item 1" />
-        <NavbarItem to="/item2" label="Item 2" />
-        <NavbarItem to="/item3" label="Item 3" />
-        <NavbarItem to="/item4" label="Item 4" />
-        <NavbarItem to="/item5" label="Item 5" />
-      </ul>
-
-      <Tienda 
-        param="navbar__shop-icon" 
-        onClick={() => console.log("Tienda clickeada")} // todo 
-      />
-    </nav>
+        <div className="navbar__wrapper">
+          <Nav />
+          <Tienda
+            param="navbar__shop-icon"
+            onClick={() => console.log("Tienda clickeada")}
+          />
+        </div>
+      </>
   );
-};
+}  
