340f2a09bbe4dd1a9624b0a022cb3f80273b2e31
raz1945 <g.sebas.sanchez@gmail.com>
2024-11-30 18:30:32 -0300

    Se agrega modal al carrito

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,30 +1,41 @@
-import React from 'react'
+import React from 'react';
 import PropTypes from 'prop-types';
-import './Card.css'
+import './Card.css';
 
-export const Card = ({ image, price, onClick }) => {
+export const Card = ({ image, price, buttonClass, onClick, isDisabled, text }) => {
   return (
     <div className='card__wrapper'>
-
       <div className='card__img-wrapper'>
         <img src={image} alt={`Imagen ${image}`} className='card__img' />
       </div>
 
       <div className='card__info'>
         <span className='card__price'>{price}</span>
-        <button type="button" className='card__btn' onClick={onClick}>
+        <button
+          type="button"
+          className={`card__btn ${isDisabled ? 'card__btn--disabled' : ''} ${buttonClass}`}
+          onClick={onClick}
+          disabled={isDisabled}
+        >
           <span className='card__btn-text'>
-            Buy now
+            {text}
           </span>
         </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
 Card.propTypes = {
   image: PropTypes.string.isRequired,
   price: PropTypes.string.isRequired,
+  buttonClass: PropTypes.string, 
   onClick: PropTypes.func.isRequired,
+  text: PropTypes.string.isRequired,
+  isDisabled: PropTypes.bool,
 };
 
+Card.defaultProps = {
+  isDisabled: false,
+  buttonClass: '',
+};
--- src/components/Collection/Collection.jsx
+++ src/components/Collection/Collection.jsx
@@ -1,21 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Card } from '../Card/Card';
+import { MiniCard } from '../miniCard/miniCard';
+
 import { LeftArrowBtn } from '../ArrowBtn/LeftArrowBtn';
 import { RightArrowBtn } from '../ArrowBtn/RightArrowBtn';
 import { collection } from '../../assets/collection';
-import './Collection.css'
+import './Collection.css';
 
-const itemsPerPage = 8; // Cantidad de items maximo por página
+const itemsPerPage = 8; // Cantidad de items máximo por página
 const allData = Object.values(collection);
 
 export const Collection = () => {
   const [currentPage, setCurrentPage] = useState(1);
+  const [cart, setCart] = useState([]);
+  const [showCartModal, setShowCartModal] = useState(false);
 
   // Cálculo de cards visibles
   const startIndex = (currentPage - 1) * itemsPerPage;
   const endIndex = startIndex + itemsPerPage;
   const visibleData = allData.slice(startIndex, endIndex);
 
+  // Controles 
   const handleNextPage = () => {
     if (currentPage < Math.ceil(allData.length / itemsPerPage)) {
       setCurrentPage((prev) => prev + 1);
@@ -28,18 +33,49 @@ export const Collection = () => {
     }
   };
 
+  // Agregar producto al carrito
+  const addToCart = (product) => {
+    setCart((prevCart) => [...prevCart, product]);
+  };
+
+  // Quita producto al carrito
+  const removeFromCart = (product) => {
+    setCart((prevCart) => {
+      const indexToRemove = prevCart.findIndex((item) => item === product);
+      if (indexToRemove !== -1) {
+        return [
+          ...prevCart.slice(0, indexToRemove),
+          ...prevCart.slice(indexToRemove + 1),
+        ];
+      }
+      return prevCart; 
+    });
+  };
+  
+
+  const totalPrice = cart.reduce((acc, item) => acc + parseFloat(item.price), 0);
+
+  // Muestra automáticamente el modal si hay productos en el carrito
+  useEffect(() => {
+    if (cart.length > 0) {
+      setShowCartModal(true);
+    }
+  }, [cart]);
+
   return (
     <>
       <div className="collection__wrapper">
-        <h1 className='collection__title'>Featured Collection</h1>
+        <h1 className="collection__title">Featured Collection</h1>
+
         <div className="collection__grid">
           {visibleData.map((item, index) => (
             <Card
               key={index}
               image={item.image}
               price={`$${item.price}`}
-
-              onClick={() => console.log(`Click en item ${index + startIndex}`)}
+              onClick={() => addToCart(item)}
+              buttonClass="card__btn"
+              text="Buy now"
             />
           ))}
         </div>
@@ -50,13 +86,42 @@ export const Collection = () => {
             onClick={handlePrevPage}
             disabled={currentPage === 1}
           />
-
           <RightArrowBtn
             onClick={handleNextPage}
             disabled={currentPage === Math.ceil(allData.length / itemsPerPage)}
           />
         </div>
       </div>
+
+
+      {/* //todo Mover */}
+      {/* Modal del carrito */}
+      {showCartModal && (
+        <div className={`cart-modal ${showCartModal ? 'show' : ''}`}>
+          <div className="cart-modal__wrapper">
+            <div className="cart-modal__product-card">
+              {cart.map((item, index) => (
+                <MiniCard
+                  key={index}
+                  image={item.image}
+                  price={`$${item.price}`}
+                  onClick={() => removeFromCart(item)}
+                  buttonClass='quit'
+                  text='Quit'
+                />
+              ))}
+              <span className="cart-modal__price">Total: ${totalPrice.toFixed(2)}</span>
+            </div>
+            <button
+              type="button"
+              className="cart-modal__close-btn"
+              onClick={() => setShowCartModal(false)}
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      )}
     </>
   );
 };
