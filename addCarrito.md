3eb78444228f1a3fbdb8a3a14a10da6470c860f1
raz1945 <g.sebas.sanchez@gmail.com>
2024-12-01 11:07:38 -0300

    correcion

---
+++ addCarrito.md
@@ -0,0 +1,12 @@
+# Add
+
+Estructura de los items {
+  id,
+  price,
+  image,
+}
+
+Estructura del carrito {
+  cant,
+  idItem
+}
--- src/assets/collection.js
+++ src/assets/collection.js
@@ -4,58 +4,72 @@ import { imgs } from './images';
 // Desde aca se puede modificar los datos de las colecciones 
 export const collection = {
   item1: {
+    id:101,
     image: imgs.collection.img1,
     price: '176.98',
   },
   item2: {
+    id:102,
     image: imgs.collection.img2,
     price: '126.98',
   },
   item3: {
+    id:103,
     image: imgs.collection.img3,
     price: '136.98',
   },
   item4: {
+    id:104,
     image: imgs.collection.img4,
     price: '146.98',
   },
   item5: {
+    id:105,
     image: imgs.collection.img1,
     price: '156.98',
   },
   item6: {
+    id:106,
     image: imgs.collection.img2,
     price: '166.98',
   },
   item7: {
+    id:107,
     image: imgs.collection.img3,
     price: '176.98',
   },
   item8: {
+    id:108,
     image: imgs.collection.img2,
     price: '166.98',
   },
   item9: {
+    id:109,
     image: imgs.collection.img3,
     price: '250.00',
   },
   item10: {
+    id:110,
     image: imgs.collection.img4,
     price: '399.99',
   },
   item11: {
+    id:111,
     image: imgs.collection.img1,
     price: '120.00',
   },
   item12: {
+    id:112,
     image: imgs.collection.img2,
     price: '300.00',
   },
   item13: {
+    id:113,
     image: imgs.collection.img3,
     price: '176.98',
   },
   item14: {
+    id:114,
     image: imgs.collection.img4,
     price: '156.98',
   },
